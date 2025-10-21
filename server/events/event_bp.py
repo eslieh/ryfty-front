33f158b37238b87b0c88e81173eab439255ac52f
eslieh <victoreslieh@gmail.com>
2025-10-21 20:32:01 +0300

    logger for events

--- server/events/event_bp.py
+++ server/events/event_bp.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, Response
 from utils.subscribe_manager import get_pubsub
 import json
+import logging
+logger = logging.getLogger(__name__)
 
 events_bp = Blueprint("events", __name__)
 
@@ -9,19 +11,19 @@ def stream(user_id):
     """
     Stream real-time events to the client via SSE (Server-Sent Events).
     """
-    print(f"[SSE] New connection for user_id: {user_id}")
+    logger.info(f"[SSE] New connection for user_id: {user_id}")
 
     pubsub = get_pubsub(user_id)  # subscribes to Redis channel user:{user_id}
-    print(f"[SSE] Subscribed to Redis channel: user:{user_id}")
+    logger.info(f"[SSE] Subscribed to Redis channel: user:{user_id}")
 
     def event_stream(pubsub):
         try:
             for message in pubsub.listen():
-                print(f"[SSE] Received message: {message}")
+                logger.info(f"[SSE] Received message: {message}")
 
                 if message["type"] == "message":
                     data = message["data"]
-                    print(f"[SSE] Sending data to client: {data}")
+                    logger.info(f"[SSE] Sending data to client: {data}")
 
                     yield f"data: {data}\n\n"
 
@@ -29,14 +31,14 @@ def stream(user_id):
                     try:
                         payload = json.loads(data)
                         if payload.get("status", {}).get("state") == "success":
-                            print("[SSE] Transaction completed — closing stream.")
+                            logger.info("[SSE] Transaction completed — closing stream.")
                             break
                     except Exception as e:
-                        print(f"[SSE] JSON decode error: {e}")
+                        logger.info(f"[SSE] JSON decode error: {e}")
         except Exception as e:
-            print(f"[SSE] Stream error: {e}")
+            logger.info(f"[SSE] Stream error: {e}")
         finally:
             pubsub.close()
-            print(f"[SSE] Closed Redis pubsub for user_id: {user_id}")
+            logger.info(f"[SSE] Closed Redis pubsub for user_id: {user_id}")
 
     return Response(event_stream(pubsub), content_type="text/event-stream")
